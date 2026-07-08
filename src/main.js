1ea01c98bd41a655f01556dce6d731ec181606ce
MaxCho <makibaypro@gmail.com>
2026-07-08 20:17:43 +0200

    Version CSS en cours, HTML fini, JS en cours

---
+++ src/main.js
@@ -0,0 +1,54 @@
+import {formaterData, cardSwitch} from "./utils.js"
+
+const btnLeft = document.querySelector(".buttonLeft");
+const btnRight = document.querySelector(".buttonRight");
+const resultsAmount = document.querySelector(".resultAmount");
+
+let centerValue = 0;
+let dataTotal = 0;
+btnLeft.disabled = true;
+
+
+formaterData("https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/stationnement-sur-voie-publique-emprises/records?limit=100")
+  .then(total => {
+    dataTotal = total;
+    cardSwitch(centerValue, dataTotal)
+    resultsAmount.textContent = `Résultats trouvés : ${dataTotal}`;
+  })
+
+/*
+// === Gestion des ID de cartes pour affichages ---
+if (appelId < 0) {
+  appelId *= -1;
+  centerId = 100 - (appelId % 100);
+} else if (appelId % 100 === 0) {   // <- pas appelId === 0
+  centerId = 100;
+} else {
+  centerId = appelId % 100;
+}
+*/
+
+
+btnLeft.addEventListener("click", () => {
+  if(centerValue > 0){
+    centerValue -= 1;
+    console.log(centerValue);
+    cardSwitch(centerValue, dataTotal);
+    btnRight.disabled = false;
+  } else {
+    btnLeft.disabled = true;
+  }
+})
+
+btnRight.addEventListener("click", () => {
+  if(centerValue < dataTotal){
+    centerValue += 1;
+    console.log(centerValue);
+    cardSwitch(centerValue, dataTotal)
+    if(centerValue > 0){
+    btnLeft.disabled = false;
+    }
+  } else if (centerValue === dataTotal - 1){
+    btnRight.disabled = true;
+  }
+})
---
+++ src/utils.js
@@ -0,0 +1,92 @@
+
+
+
+// Liens site : https://opendata.paris.fr/explore/dataset/stationnement-sur-voie-publique-emprises/information/?disjunctive.regpri&disjunctive.regpar&disjunctive.typsta&disjunctive.arrond&disjunctive.locsta&disjunctive.zoneres&disjunctive.parite&disjunctive.signhor&disjunctive.signvert&disjunctive.confsign&disjunctive.typemob&sort=-regpri&basemap=jawg.dark&location=19,48.88298,2.38301
+// API : https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/stationnement-sur-voie-publique-emprises/records?limit=100
+
+const cardsGRP = document.querySelector(".cardsGRP");
+
+
+
+// --- FONCTIONS ---
+
+const addID = (dataArray) => {
+  const dataID = [];
+  for(let i = 0; i < dataArray.length;i++){
+    dataID.push({...dataArray[i], id: i});
+  }
+  return dataID;
+}
+
+const insertCard = (parkingSlot) => {
+  let img = "";
+  if(parkingSlot.regpri === "LIVRAISON") img = "./src/assets/truck.png";
+  else if(parkingSlot.regpri === "2 ROUES") img = "./src/assets/motorcycle.png";
+  else if(parkingSlot.regpri === "GIG/GIC") img = "./src/assets/wheelChair.png"
+  else if(parkingSlot.regpri === "PAYANT MIXTE" || parkingSlot.regpri === "PAYANT ROTATIF") img = "./src/assets/car.png";
+  else if(parkingSlot.regpri === "AUTRE REGIME") img = "./src/assets/car.png";
+
+  let card = `<article class="card" data-id="${parkingSlot.id}">
+        <h2>${parkingSlot.typevoie} ${parkingSlot.nomvoie}</h2>
+        <img src="${img}" alt="PictoPlace" class="imgSize"/>
+        <dl class="infos" >
+          <dt class="infosType" >Type de place :</dt>
+          <dd class="infosData">${parkingSlot.regpri}</dd>
+
+          <dt class="infosType" >Disposition :</dt>
+          <dd class="infosData">${parkingSlot.typsta}</dd>
+
+          <dt class="infosType" >Longueur :</dt>
+          <dd class="infosData">${parkingSlot.lon.toFixed(2)} mètre</dd>
+
+          <dt class="infosType" >Largeur :</dt>
+          <dd class="infosData">${parkingSlot.lar} mètre</dd>
+
+          <dt class="infosType" >Nb. de place (Approx):</dt>
+          <dd class="infosData">${parkingSlot.plarel}</dd>
+        </dl>
+  </article>`
+  cardsGRP.insertAdjacentHTML("beforeend", card);
+};
+
+export const dataTotal = async (link) => {
+  try {
+    const response = await fetch(link);
+    const data = await response.json();
+    const dataArray = data.results;
+    const dataTotal = dataArray.length;
+  } catch (error){
+    console.error(error.message);
+  }
+  return console.log(dataTotal);
+}
+
+//--- Fonction Selection Card ---
+export const cardSwitch = (centerValue, dataTotal) => {
+
+}
+
+// --- Recuperation des donnés ---
+export const formaterData = async (link) => {
+  try {
+    const response = await fetch(link);
+    const data = await response.json();
+    console.log("data :" + data);
+
+    const dataArray = data.results;
+    console.log("dataArray :" + dataArray);
+
+    const dataTotal = dataArray.length;
+
+    const dataFinal = addID(dataArray);
+    console.log("dataID :" + dataFinal[2].id);
+
+    dataFinal.forEach(parkingSlot => {
+      insertCard(parkingSlot);
+    });
+
+    return dataTotal
+  } catch (error){
+    console.error(error.message);
+  }
+}
